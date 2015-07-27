74e8e741f4424e173223a9629e22023f090c1fdb
Damon Maldonado <damon@mpwmarketing.com>
2015-07-27 15:13:41 -0400

    quick links style, plugin includes

--- functions.php
+++ functions.php
@@ -117,5 +117,78 @@ function insert_layer_slider () {
 
 add_action( 'tha_entry_before', 'insert_layer_slider', 10 );
 
+	require_once 'includes/class-tgm-plugin-activation.php';
+	
+function my_theme_register_required_plugins() {
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
+
+		// This is an example of how to include a plugin from a GitHub repository in your theme.
+		// This presumes that the plugin code is based in the root of the GitHub repository
+		// and not in a subdirectory ('/src') of the repository.
+		array(
+			'name'      => 'MPW Quick Links',
+			'slug'      => 'mpw-quick-links',
+			'source'    => 'https://github.com/damonmaldonado/mpw-quick-links/archive/master.zip',
+			'required' => true,
+		),
+
+		// This is an example of how to include a plugin from the WordPress Plugin Repository.
+		array(
+			'name'      => 'Meta Box',
+			'slug'      => 'meta-box',
+			'required'  => true,
+		),
+
+		// This is an example of the use of 'is_callable' functionality. A user could - for instance -
+		// have WPSEO installed *or* WPSEO Premium. The slug would in that last case be different, i.e.
+		// 'wordpress-seo-premium'.
+		// By setting 'is_callable' to either a function from that plugin or a class method
+		// `array( 'class', 'method' )` similar to how you hook in to actions and filters, TGMPA can still
+		// recognize the plugin as being installed.
+		array(
+			'name'        => 'WordPress SEO by Yoast',
+			'slug'        => 'wordpress-seo',
+			'is_callable' => 'wpseo_init',
+		),
+
+	);
+
+	/*
+	 * Array of configuration settings. Amend each line as needed.
+	 *
+	 * TGMPA will start providing localized text strings soon. If you already have translations of our standard
+	 * strings available, please help us make TGMPA even better by giving us access to these translations or by
+	 * sending in a pull-request with .po file(s) with the translations.
+	 *
+	 * Only uncomment the strings in the config array if you want to customize the strings.
+	 */
+	$config = array(
+		'id'           => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'parent_slug'  => 'themes.php',            // Parent menu slug.
+		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+
+
+	);
+
+	tgmpa( $plugins, $config );
+}
+
+add_action( 'tgmpa_register', 'my_theme_register_required_plugins' );
+
+
+
 // Add a hook for child themes to execute code.
 do_action( 'flagship_after_setup_parent' );
+
+
